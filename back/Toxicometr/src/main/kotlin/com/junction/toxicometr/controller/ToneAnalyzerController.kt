f3e88e4bd58bab5e34da74f2f36a8c0fd65e266d
wildspruce <m.elnara.397@gmail.com>
2021-11-20 22:46:31 +0300

    Cleanup: removed extra code

--- back/Toxicometr/src/main/kotlin/com/junction/toxicometr/controller/ToneAnalyzerController.kt
+++
@@ -1,9 +0,0 @@
-package com.junction.toxicometr.controller
-
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-@CrossOrigin
-class ToneAnalyzerController {
-}
--- front/toxicometr/src/router.js
+++ front/toxicometr/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ChatPage from "./components/ChatPage";
-import HighlightingExample from './components/HighlightingExample.vue'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ let router = new Router({
             path: '/chat',
             name: 'Chat',
             component: ChatPage
-        },
-        {
-            path: '/hw',
-            name: 'HighlightingExample',
-            component: HighlightingExample
         }
     ]
 })
