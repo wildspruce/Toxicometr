a9248d1b94a1da3fb8c4d30052004c1537c13dda
lesslyrics <alina.boshchenko@jetbrains.com>
2021-11-21 04:27:54 +0300

    added ToneController

--- back/Toxicometr/src/main/kotlin/com/junction/toxicometr/model/OutgoingMessage.kt
+++ back/Toxicometr/src/main/kotlin/com/junction/toxicometr/model/OutgoingMessage.kt
@@ -1,19 +1,15 @@
 package com.junction.toxicometr.model
 
-import com.junction.toxicometr.emojiGenerator.Replacement
 import com.junction.toxicometr.toneAnalyzer.TextTone
 
-class OutgoingMessage {
+class OutgoingMessage : OutgoingMessageDefault {
     var text: String? = null
         private set
 
     var replacement: String = ""
 
-    var tone : TextTone? = null
 
-    constructor() {}
-
-    constructor(content: String?, replacement: String, tone: TextTone?) {
+    constructor(content: String?, replacement: String, tone: TextTone?) : super(tone) {
         this.text = content
         this.replacement = replacement
         this.tone = tone;
